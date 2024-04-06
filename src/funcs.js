9b3119fcd3048100e517c39b2b16985039faf0f5
mindprocessor <62874871+mindprocessor@users.noreply.github.com>
2024-04-06 19:47:34 +0800

    added multiple functions

--- src/funcs.js
+++ src/funcs.js
@@ -11,3 +11,22 @@ function write_team_b(){
     let name = document.getElementById('team-b-name').textContent;
     fs.writeFile('./team_b_name.txt', name, err=>{});
 }
+
+
+function write_team_a_score(score){
+    let score_str = score.toString();
+    fs.writeFile('./team_a_score.txt', score_str, err=>{});
+}
+
+function write_team_b_score(score){
+    let score_str = score.toString();
+    fs.writeFile('./team_b_score.txt', score_str, err=>{});
+}
+
+function write_team_a_foul(foul){
+    fs.writeFile('./team_a_foul.txt', foul, err=>{});
+}
+
+function write_team_b_foul(foul){
+    fs.writeFile('./team_b_foul.txt', foul, err=>{});
+}
